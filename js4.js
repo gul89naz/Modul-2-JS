2c7ecf86c377455b29e6a81c5a98b101a2383598
Gulnaz Alagozova <89704754+gul89naz@users.noreply.github.com>
2022-01-30 18:13:10 +0300

    Delete js4.js

--- js4.js
+++
@@ -1,8 +0,0 @@
-let weightSouvenirs=75;
-let weightBauble=112;
-
-let countSouvenirs=prompt("Введите количество сувениров");
-let countBauble= prompt("Введите количество безделушек");
-
-let totalWeight = countSouvenirs * weightSouvenir + countBauble * weightBauble;
-document.write(`Общий вес - ${totalWeight} граммов`);
